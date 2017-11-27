9fd2e20ad180a966bbc94b2576dfe583d9916a0a
park-se-jun <qkrtpwns1999@naver.com>
2017-11-27 13:33:25 +0900

    get_command추가
    작동 안되면 이전버전으로 되돌리셈

--- JunsPartFinal.c
+++ JunsPartFinal.c
@@ -53,6 +53,51 @@ char* changeLogColon(char*);
 void linkAnotherMovie(void);
 void linkAnotherDirector(void);
 void linkAnotherActor(void);
+void get_command(char* input)
+{
+	char* commands, *mda, *option, *num;
+	input = (char*)malloc(30);
+	gets(input);
+	commands = strtok(input, " ");
+	mda = strtok(NULL, " ");
+	if (strcmp(commands, "add") == 0){
+    if (mda == 0) {
+      printf("@@ Please input the option!\n\n");
+      free(input);
+			fclose(mp);
+			return ;
+    }
+		add(mda);
+		fclose(mp);
+	}
+	else if (strcmp(commands, "update") == 0){
+    printf("업데이트 코드");
+    exit(1);
+	}
+	else if (strcmp(commands, "delete") == 0){
+    printf("딜리트 코드");
+    exit(1);
+	}
+	else if (strcmp(commands, "search") == 0){
+    printf("서치 코드");
+    exit(1);
+	}
+	else if (strcmp(commands, "print") == 0){
+    printf("프린트 코드");
+    exit(1);
+	}
+	else if (strcmp(commands, "sort") == 0){
+    printf("소트 코드");
+    exit(1);
+	}
+	else if (strcmp(commands, "save") == 0){
+    printf("세이브 코드");
+    exit(1);
+	}
+	else if (strcmp(commands, "end") == 0){
+		exit(1);
+	}
+}
 
 int main(void)
 {
@@ -74,20 +119,9 @@ int main(void)
 		mp = fopen("movie_log", "at");
 		dp = fopen("director_log", "at");
 		ap = fopen("actor_log", "at");
-		input = (char*)malloc(30);
 		printf("(movie) ");
-		gets(input);
-		commands = strtok(input, " ");
-		mda = strtok(NULL, " ");
-		if (mda == 0) {
-			printf("@@ Please input the option!\n\n");
-			free(input);
-			continue;
-		}
-		if (strcmp(commands, "add") == 0) {
-			add(mda);
+		get_command(input);
 
-		}
 		free(input);
 	}
 
